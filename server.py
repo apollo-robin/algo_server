05e09af6d3c4b27b7271063bb5c7a204c64c9e8d
Robin <robinraj055@gmail.com>
2021-11-25 04:58:01 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,89 +1,70 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Sep 17 20:36:29 2021
+Created on Thu Nov 25 00:38:51 2021
 
 @author: robin
 """
-
-from google.cloud import firestore
-from google.oauth2 import service_account
-import json
+import pandas as pd
+from joblib import load
+import warnings 
+warnings.filterwarnings("ignore")
 import streamlit as st
 
-# Authenticate to Firestore 
-key_dict = json.loads(st.secrets["textkey"])
-creds = service_account.Credentials.from_service_account_info(key_dict)
-db = firestore.Client(credentials=creds, project="streamlit-algotrade")
 
-
-#Function to check if user is a premium user 
-def is_premium(username,db):
-    data_ref = db.collection("premium_users").document(username)
-    data = data_ref.get()
-    if data.exists:
-        return True
-    else:
-        return False
-
-#Function to add premium user 
-def create_user(username,db):
-    data_ref = db.collection("upgrade").document(username)
-    data = data_ref.get().to_dict()
+class CreditChurnClassification():
     
-    if not is_premium(username,db):     
-        if data['PremTransID'] == "12345678":  
-            date = {"SubsDate":data['SubsDate']}
-            null = {}
-            pf_data = {"Portfolio_1": "Portfolio 1",
-                       "Portfolio_2": "Portfolio 2",
-                       "Portfolio_3": "Portfolio 3"
-                       }
-             
-            db.collection('premium_users').document(username).collection("Portfolio_1").document(username).set(null)
-            db.collection('premium_users').document(username).collection("Portfolio_2").document(username).set(null)
-            db.collection('premium_users').document(username).collection("Portfolio_3").document(username).set(null)
-            db.collection('premium_users').document(username).collection("paper_trading").document(username).set(null)
-            db.collection('premium_users').document(username).collection("pf_name").document("Name").set(pf_data)
-            db.collection('premium_users').document(username).collection("watchlist").document(username).set(null)
-            db.collection('premium_users').document(username).set(date)
-            
-    db.collection("upgrade").document(username).delete()
-
-
-#Function to get valid premium users
-def get_users(db):
-    valid_prem = []
-    invalid_user = []
-    users = db.collection("upgrade").stream()
-    
-    for user in users:
-        temp = user.to_dict()
-        if temp["PremTransID"] == "12345678":
-            valid_prem.append(user.id)
-        else:
-            invalid_user.append(user.id)
-    
-    return valid_prem, invalid_user
+    def __init__(self, model, features, acc):
+        self.model = model
+        self.features = features
+        self.acc = acc
         
-    
+
+
+prediction_model = load('../Models/Churn_xgb_top.pkl')
 
 #Setting up page configuration
-st.set_page_config(page_title= 'apollo_server' ) 
-st.markdown('<p style= "font-weight: bold; text-align:left; color: #1f4886; font-family:Segoe Script; font-size: 44px"> apollo <p>', unsafe_allow_html=True)
-st.markdown("<hr>", unsafe_allow_html=True)
+st.set_page_config(page_title= 'Group 3', initial_sidebar_state='auto') 
+
+st.markdown('<p style= "font-weight: bold; color: #1f4886; text-align:center; font-family:Segoe Script; font-size: 44px"> Credit Card Attrition <p>', unsafe_allow_html=True  )
+st.markdown('<p style= "text-align:center; color: black;  font-size: 18px">We intend to solve the problem of cusotmer attrition by prediciting customers who are likely churn.  <p>', unsafe_allow_html=True) 
 
 
-update = st.button("Update Premium Users")
-
-if update:
-    valid,invalid = get_users(db)
-    st.write("Following Premium users were added:")
-    for user in valid:
-        st.write(user)
-            
-    for user in valid:
-        create_user(user, db)
+with st.form("info"):
+    st.markdown("**Just fill in the details and we'll let you know** :yum: ")
     
-    for user in invalid:
-        db.collection("upgrade").document(user).delete()
-        
+    col1 , col2 = st.beta_columns(2)
+    age = col1.slider('Age', min_value=18,max_value=75)
+    gender = col2.selectbox('Gender', options=("M","F"))
+    total_rel_cnt = col2.number_input("Total Relationship Count",value = 5)
+    mth_inactive = col1.number_input("Months Inactive", value = 1)
+    tot_rev_bal = col2.number_input("Total Revolving Balance",value = 1000)
+    amt_chng_Q4_1 = col1.number_input("Total Amnout Change Q4-Q1",value = 0.4 )
+    trans_amt = col2.number_input("Total Transaction Amount", value = 1000 )
+    trans_cnt = col1.number_input("Total Transaction Count", value = 50)
+    cnt_chng_Q4_1 = col2.number_input("Total Transaction Count Change Q4-Q1", value =0.4 )
+    avg_util = col1.number_input("Average Utilisation Ratio", value = 0.5)
+    
+    submit = st.form_submit_button("Submit")
+    
+
+if submit:
+    data = {'Customer_Age':age,
+            'Gender': gender,
+           'Total_Relationship_Count':total_rel_cnt,
+           'Months_Inactive_12_mon':mth_inactive,
+           'Total_Revolving_Bal':tot_rev_bal,
+           'Total_Amt_Chng_Q4_Q1':amt_chng_Q4_1,
+           'Total_Trans_Amt':trans_amt,
+           'Total_Trans_Ct':trans_cnt,
+           'Total_Ct_Chng_Q4_Q1':cnt_chng_Q4_1,
+           'Avg_Utilization_Ratio':avg_util}
+    
+    X = pd.DataFrame(data, index = [0])
+    attrition = prediction_model.model.predict(X)
+    
+    if attrition[0] == 0:
+        st.balloons()
+        st.success("The customer is not likely to churn !")
+    else:
+        st.error("This customer may attrite. Do something !")
+       
